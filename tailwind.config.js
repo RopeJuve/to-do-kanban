6b601a16ee8e675c50cb829c9b3d12f742ac6222
RopeJuve <rope.s@hotmail.com>
2024-04-06 18:04:26 +0200

    Creating the Board UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Jakarta': ['Jakarta', 'sans-serif'],
+      },
+      gridTemplateColumns:{
+        '2': '30% 70%',
+        '3': 'repeat(3, 280px)',
+      },
+      gridTemplateRows:{
+        '2': '80px 90%' 
+      }
+    },
   },
   plugins: [],
 }
