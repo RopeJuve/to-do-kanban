0ffac4234c3b26519128ab31f3e0ad4330f684b9
RopeJuve <rope.s@hotmail.com>
2024-04-08 21:50:27 +0200

    hide Side bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ module.exports = {
       fontFamily: {
         'Jakarta': ['Jakarta', 'sans-serif'],
       },
-      gridTemplateColumns:{
-        '2': 'minmax(260px, 300px) 1fr',
+      gridTemplateColumns: {
+        '1': '0px auto',
+        '2': 'minmax(260px, 300px) auto',
         '3': 'repeat(3, 280px)',
       },
-      gridTemplateRows:{
-        '2': '80px 90%' 
+      gridTemplateRows: {
+        '2': '80px 90%'
       }
     },
   },
