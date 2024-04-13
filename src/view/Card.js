86c571f4b3ec6f17a370b059c8a495628d110efe
RopeJuve <rope.s@hotmail.com>
2024-04-13 17:58:35 +0200

    Add dropable to cards

--- src/view/Card.js
+++ src/view/Card.js
@@ -6,6 +6,7 @@ export const createCard = (task) => {
   const completedSubTasks = subTasks.filter((subTask) => subTask.content.isCompleted).length;
   const card = document.createElement('div');
   card.dataset.id = id;
+  card.draggable = true;
   card.className = 'px-[1rem] py-[1.5rem] bg-[#2B2C37] rounded-[0.5rem] cursor-pointer hover:bg-[#3A3B4A] transition-all duration-300 ease-in-out';
   card.innerHTML = `
     <h2 class="text-white font-[600] tracking-wide mb-[0.5rem] pointer-events-none">
@@ -14,5 +15,13 @@ export const createCard = (task) => {
     <span class="text-white opacity-50 text-[0.875rem] pointer-events-none"
       >${completedSubTasks} of ${subTasks.length} subtasks</span
     >`;
+
+    card.addEventListener("dragstart", (e) => {
+      e.dataTransfer.setData("text/plain", id);
+      console.log("dragstart", id);
+    });
+    card.addEventListener("dragend", (e) => {
+      console.log("dragend", id);
+    });
   return card;
 }
